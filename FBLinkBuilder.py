62482bdb5c6d4be3b8aae731414430f568552edb
V Developers <virdwait@gmail.com>
2020-10-25 15:49:09 +0530

    Fixed minor bug in link export.

--- FBLinkBuilder.py
+++ FBLinkBuilder.py
@@ -7,7 +7,7 @@ from datetime import datetime
 fname = datetime.now().strftime("FB_Deeplinks%d%m%Y%H%M%S.txt") #default filename
 
 parser = argparse.ArgumentParser() 
-parser.add_argument('-i', help='Facebook APK file')
+parser.add_argument('-i', help='Input file: Facebook APK file')
 parser.add_argument('-o', help='Output file', nargs='?', default=fname)
 parser.add_argument('-e', help='Only show exported. Defaulted to False', nargs='?', default=False)
 args = parser.parse_args()
@@ -17,7 +17,7 @@ output_name = args.o #generated output / provided
 exported = args.e #False / provided
 
 with ZipFile(file_name, 'r') as zip: 
-    print('Extracting native routes file...') #fyi
+    print('Extracting native routes file...')
     
     data = zip.read('assets/react_native_routes.json') #extract file from zip
     js = json.loads(data.decode("utf-8")) #to read as list
@@ -28,7 +28,7 @@ with ZipFile(file_name, 'r') as zip:
     
     text_file = open(output_name, "w") #open output
 
-    print('Manipulating data...') #fyi
+    print('Manipulating data...')
     for key in js: #for each block in json
         for key2 in key['paramDefinitions']: #grab the collection of params
             params += key2 + '=' + str(key['paramDefinitions'][key2]['type']).upper() + '&' #append params with type
@@ -38,7 +38,7 @@ with ZipFile(file_name, 'r') as zip:
                 params = '' #Reset params
                 continue #try next block
                 
-        link = 'fb:/' + key['path'] + '/?' + params #build link
+        link = 'fb://' + key['path'] + '/?' + params #build link
         print(link[:-1]) #fyi
         text_file.write(link[:-1]+ '\n') #write to file
         i += 1 #increase counter
